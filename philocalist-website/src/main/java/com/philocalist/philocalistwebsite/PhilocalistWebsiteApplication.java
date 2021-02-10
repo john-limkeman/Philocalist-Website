aaaeb39be199044695cdabbed9fcec91247022c2
John Limkeman <john.limkeman@gmail.com>
2021-02-10 11:32:08 -0600

    starting project

---
+++ philocalist-website/src/main/java/com/philocalist/philocalistwebsite/PhilocalistWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.philocalist.philocalistwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilocalistWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilocalistWebsiteApplication.class, args);
+	}
+
+}
---
+++ philocalist-website/src/test/java/com/philocalist/philocalistwebsite/PhilocalistWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philocalist.philocalistwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilocalistWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
