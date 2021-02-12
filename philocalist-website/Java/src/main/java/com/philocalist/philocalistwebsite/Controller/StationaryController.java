1407c2062659cba63a6c3fc081ad674745fd35cb
John Limkeman <john.limkeman@gmail.com>
2021-02-12 15:26:43 -0600

    Controller Made, Vue started
    Difficulties getting Vue dependencies installed, will need to circle back on that

--- philocalist-website/Java/src/main/java/com/philocalist/philocalistwebsite/Controller/StationaryController.java
+++ philocalist-website/Java/src/main/java/com/philocalist/philocalistwebsite/Controller/StationaryController.java
@@ -1,11 +1,27 @@
 package com.philocalist.philocalistwebsite.Controller;
 
+import com.philocalist.philocalistwebsite.DAO.StationaryDAO;
+import com.philocalist.philocalistwebsite.Model.Stationary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 public class StationaryController {
+
+    @Autowired
+    StationaryDAO dao;
+
+    @RequestMapping(path = "/allStationary", method = RequestMethod.GET)
+    public List<Stationary> getAllStationary(){
+        return dao.getAllStationary();
+    }
+
 
 
 }
---
+++ philocalist-website/Vue/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+      <div id="nav">
+        <router-link class="navlink" v-bind:to= "{ name: 'home'}"> Home </router-link> &nbsp;|&nbsp;
+        <router-link class="navlink" v-bind:to= "{ name: 'wedding-invites'}"> Wedding Invites </router-link> &nbsp;|&nbsp;
+        <router-link class="navlink" v-bind:to= "{ name: 'save-the-dates'}"> Save the Dates</router-link> &nbsp;|&nbsp;
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
