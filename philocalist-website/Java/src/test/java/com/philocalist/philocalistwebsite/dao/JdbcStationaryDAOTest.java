4e60962fcc3afc02c3e9d64781ee77b02140549f
John Limkeman <john.limkeman@gmail.com>
2021-02-23 11:43:44 -0600

    updated dband set up testing db
    did away with the array level postgres variables, generated application.properties file for the testing environment
    
    Will need to test to make sure its connected properly

---
+++ philocalist-website/Java/src/test/java/com/philocalist/philocalistwebsite/dao/JdbcStationaryDAOTest.java
@@ -0,0 +1,40 @@
+package com.philocalist.philocalistwebsite.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JdbcStationaryDAOTest {
+
+    @Test
+    void addStationary() {
+    }
+
+    @Test
+    void deleteStationary() {
+    }
+
+    @Test
+    void updateStationary() {
+    }
+
+    @Test
+    void getAllStationary() {
+    }
+
+    @Test
+    void getStationaryByCategory() {
+    }
+
+    @Test
+    void getStationaryByTheme() {
+    }
+
+    @Test
+    void getStationaryByPrintType() {
+    }
+
+    @Test
+    void getStationaryById() {
+    }
+}
