46c7ca847e7dcde931e964269695b32e6278befa
John Limkeman <john.limkeman@gmail.com>
2021-03-04 10:30:44 -0600

    quantities working with shopping cart

--- philocalist-website/Vue/src/components/CartListing.vue
+++ philocalist-website/Vue/src/components/CartListing.vue
@@ -2,10 +2,10 @@
   <div>
 <h2>cart items</h2>  
 <div v-for="item in Cart" v-bind:key="item.id">
-<StationaryCard v-bind:isCart="true" v-bind:id="item.id"/>
-<label for="itemQuantity">Count: </label>
-<input v-bind:value="changeNum.quantity" name="itemQuantity" type="number" min="0" max="1000" id="quantityInput">
-<button v-on:click="updateQuantity(item.id)">Update</button>
+    <StationaryCard v-bind:isCart="true" v-bind:id="item.id"/>
+    <label for="itemQuantity">Count: </label>
+    <input name="itemQuantity" type="number" min="0" max="1000" v-bind:id='item.id'>
+    <button v-on:click="updateQuantity(item.id)">Update</button>
 </div>
 </div>
 </template>
@@ -15,10 +15,7 @@ import StationaryCard from '../components/StationaryCard.vue'
 export default {
 data(){
     return{
-        // changeNum: {
-        //     id: 0,
-        //     quantity: 0
-        // }
+        
     }
 },
 components: {
@@ -26,18 +23,17 @@ components: {
 },
 computed: {
     Cart(){
-    
         return this.$store.state.cart;
     }
 },
 methods : {
-    // updateQuantity(id){
-    //     let quantityPayload = {
-    //         id: id,
-    //         quantity: this.changeNum
-    //     }
-    //     this.$store.dispatch('updateQuantity', {quantityPayload})
-    // }
+    updateQuantity(id){
+        let quantityPayload = {
+            id: id,
+            quantity: document.getElementById(id).value
+        }
+        this.$store.dispatch('updateQuantity', quantityPayload)
+    }
 }
 }
 </script>
