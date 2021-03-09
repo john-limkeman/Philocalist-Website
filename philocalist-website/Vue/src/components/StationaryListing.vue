1fbe75c206ce1c6a0e6d393b16599fe10ad98f91
John Limkeman <john.limkeman@gmail.com>
2021-03-09 14:05:39 -0600

    modal css added

--- philocalist-website/Vue/src/components/StationaryListing.vue
+++ philocalist-website/Vue/src/components/StationaryListing.vue
@@ -8,18 +8,21 @@
       <StationaryCard  v-bind:id="item.id" v-bind:isCart="false" v-on:details="toggleModal(item)"/>
     </div>
 
+<!-- 
+Modal + Overlay shows more details for each stationary item when clicked 
+-->
     <transition>
-      <div class="modal-overlay" v-if="showModal" @click="toggleModal()"></div>
+      <div class="modalOverlay" v-if="showModal" @click="toggleModal()"></div>
     </transition>
     <transition>
       <div class="modal" v-if="showModal">
+        <button class="modalExitBtn" v-on:click="toggleModal()">X</button>
         <h2 class="modalTitle">{{modalContent.title}}</h2>
         <img v-bind:src="modalContent.imgURL" alt="error displaying image" class="modalImage">
         <p>Price: ${{modalContent.price}} <br/>
           Theme: {{modalContent.theme}} <br/>
           Print Type: {{modalContent.printType}} <br/>
           <button class="modalAddBtn"></button>
-          <button class="modalExitBtn" v-on:click="toggleModal()">X</button>
         </p>
 
       </div>
@@ -63,6 +66,54 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
+/* Modal Styling */
+.modalOverlay{
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 98;
+  background-color: rgba(255,255,255,0.8);
+}
+.modal{
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 99;
+
+  width: 50%;
+  max-width: 400px;
+  height: 60%;
+  padding: 15px;
+
+  background-color: white;
+  border: 3px solid #FFD700;
+  border-radius: 10px;
+
+}
+.modal img{
+  max-height: 70%;
+  max-width: 70%;
+}
+
+.modalExitBtn{
+  float: right;
+  font-size: 22px;
+  border: none;
+  background: none;
+
+  }
+.modalExitBtn:hover{
+    color: red;
+  }
+
+
+#container{
+  height: 100%;
+  width: 100%;
+}
 </style>
