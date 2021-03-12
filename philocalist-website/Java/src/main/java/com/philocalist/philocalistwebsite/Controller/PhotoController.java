8349e91bc5fb51164761d3ba96c7963dd86a194f
John Limkeman <john.limkeman@gmail.com>
2021-03-12 15:47:50 -0600

    finished Photo Controller

--- philocalist-website/Java/src/main/java/com/philocalist/philocalistwebsite/Controller/PhotoController.java
+++ philocalist-website/Java/src/main/java/com/philocalist/philocalistwebsite/Controller/PhotoController.java
@@ -1,7 +1,53 @@
 package com.philocalist.philocalistwebsite.controller;
 
-import org.springframework.stereotype.Controller;
 
-@Controller
+import com.philocalist.philocalistwebsite.dao.PhotoDAO;
+import com.philocalist.philocalistwebsite.model.Photo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class PhotoController {
+
+    @Autowired
+    PhotoDAO dao;
+
+    /* GET ALL  */
+    @RequestMapping(path = "/photos", method= RequestMethod.GET)
+    public List<Photo> getAllPhotos(){
+        return dao.getAllPhotos();
+    }
+
+    /* GET ALL BY STATIONARY ID */
+    @RequestMapping(path = "/photos/stationary/{id}", method = RequestMethod.GET)
+    public List<Photo> getPhotosByStationary(@PathVariable int id){
+        return dao.getPhotoByStationaryId(id);
+    }
+
+    /* GET PHOTO BY PHOTO ID */
+    @RequestMapping(path = "/photos/{id}", method = RequestMethod.GET)
+    public Photo getPhotoById(@PathVariable int id){
+        return dao.getPhotoById(id);
+    }
+
+    /* ADD PHOTO */
+    @RequestMapping(path = "/photos", method = RequestMethod.POST)
+    public void addPhoto(@RequestBody Photo photo){
+        dao.addPhoto(photo);
+    }
+
+    /* UPDATE PHOTO */
+    @RequestMapping(path = "/photos/{id}", method = RequestMethod.PUT)
+    public void updatePhoto(@RequestBody Photo photo, @PathVariable int id){
+        dao.updatePhoto(photo, id);
+    }
+
+    /* DELETE PHOTO */
+    @RequestMapping(path = "/photos/{id}", method = RequestMethod.DELETE)
+    public void deletePhoto(@PathVariable int id){
+        dao.deletePhoto(id);
+    }
 }
