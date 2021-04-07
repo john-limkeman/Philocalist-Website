0b0a05e0781ec07be1a72e07ff737691c0fb4783
John Limkeman <john.limkeman@gmail.com>
2021-04-07 15:38:04 -0500

    added jwt and other security classes

--- philocalist-website/Java/Database/index.sql
+++ philocalist-website/Java/Database/index.sql
@@ -4,6 +4,22 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS Photos;
 DROP TABLE IF EXISTS Stationaries;
 DROP TABLE IF EXISTS Themes;
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS seq_user_id;
+
+CREATE SEQUENCE seq_user_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+  
+ CREATE TABLE users (
+	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
+	username varchar(50) NOT NULL,
+	password_hash varchar(200) NOT NULL,
+	role varchar(50) NOT NULL,
+	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
 
 CREATE Table Themes(
 id serial,
