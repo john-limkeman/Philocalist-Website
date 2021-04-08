720a390ee35efa94d013346d0ca641b47f4b2f58
John Limkeman <john.limkeman@gmail.com>
2021-04-08 15:00:19 -0500

    adjusted db script

--- philocalist-website/Java/Database/index.sql
+++ philocalist-website/Java/Database/index.sql
@@ -21,7 +21,7 @@ CREATE SEQUENCE seq_user_id
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('beth','$2a$10$ZgjBSuT4iByZtiPDX0cAteqpEDsfsEGFnrjpjMqebxsDyWhrA9ckG','ROLE_ADMIN');
 
 CREATE Table Themes(
 id serial,
